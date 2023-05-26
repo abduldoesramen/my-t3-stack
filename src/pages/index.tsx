b0060e6966246c7c38b85d24b5ce76e1ae3573b7
Abdulrehman Ahmad <abdulrehman821337@gmail.com>
2023-05-26 10:59:44 +1000

    Added feature to render post from Prisma db

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,8 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
-
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -22,6 +20,11 @@ const Home: NextPage = () => {
           {!!user.isSignedIn && <SignOutButton />}
         </div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
